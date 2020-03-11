93b61c7409c2933f65412fe31438f77397a98bae
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-12 12:04:33 +1300

    1 file modified, 1 added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+  apiKey: "AIzaSyAIae2xJEZMbIyaTQ2ht4CnI0UHbJtL6fI",
+  authDomain: "https://react-reminder-2.firebaseapp.com/?80876",
+  databaseURL: "https://react-reminder-2.firebaseio.com/?80876",
+  storageBucket: "react-reminder-2.appspot.com",
+  messagingSenderId: "130650036145"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
