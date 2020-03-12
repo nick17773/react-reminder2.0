f86855b6a6071b2047d6d2d97ba2cda29a78b22b
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-13 09:19:31 +1300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./firebase.js";
 
-import './App.css';
-import firebase from './firebase';
-
-class ReminderFix extends Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentItem: '',
-      username: '',
+      currentItem: "",
+      username: "",
       items: []
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -21,20 +21,20 @@ class ReminderFix extends Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
+    const itemsRef = firebase.database().ref("items");
     const item = {
       title: this.state.currentItem,
       user: this.state.username
-    }
+    };
     itemsRef.push(item);
     this.setState({
-      currentItem: '',
-      username: ''
+      currentItem: "",
+      username: ""
     });
   }
   componentDidMount() {
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshot) => {
+    const itemsRef = firebase.database().ref("items");
+    itemsRef.on("value", snapshot => {
       let items = snapshot.val();
       let newState = [];
       for (let item in items) {
@@ -55,40 +55,54 @@ class ReminderFix extends Component {
   }
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <header>
-            <div className="wrapper">
-              <h1>Fun Food Friends</h1>
-                             
-            </div>
+          <div className="wrapper">
+            <h1>Fun Food Friends</h1>
+          </div>
         </header>
-        <div className='container'>
-          <section className='add-item'>
-                <form onSubmit={this.handleSubmit}>
-                  <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                  <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                  <button>Add Item</button>
-                </form>
+        <div className="container">
+          <section className="add-item">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                name="username"
+                placeholder="What's your name?"
+                onChange={this.handleChange}
+                value={this.state.username}
+              />
+              <input
+                type="text"
+                name="currentItem"
+                placeholder="What are you bringing?"
+                onChange={this.handleChange}
+                value={this.state.currentItem}
+              />
+              <button>Add Item</button>
+            </form>
           </section>
-          <section className='display-item'>
-              <div className="wrapper">
-                <ul>
-                  {this.state.items.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>brought by: {item.user}
-                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                        </p>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
+          <section className="display-item">
+            <div className="wrapper">
+              <ul>
+                {this.state.items.map(item => {
+                  return (
+                    <li key={item.id}>
+                      <h3>{item.title}</h3>
+                      <p>
+                        brought by: {item.user}
+                        <button onClick={() => this.removeItem(item.id)}>
+                          Remove Item
+                        </button>
+                      </p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </section>
         </div>
       </div>
     );
   }
 }
-export default ReminderFix;
+export default App;
