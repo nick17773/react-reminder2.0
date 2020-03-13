ae761a837142a2992759cf1935cefb9fa2359d65
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-13 13:28:30 +1300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     return (      
        <BrowserRouter>
         <div>
+        <br/>
           <Navigation />
             <Switch>
              <Route path="/" component={Home} exact/>
