4477053f7e769aa4fe92b675bb08621905eaec0a
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-14 09:58:24 +1300

    1 file modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,16 @@ const home = () => {
     return (
        <div>
        <header>
+       <div className="wrapper">
           <h1>Home</h1>
+          </div>
           </header>
+          <div className="container">
            <p>This is a home page, not much to see here.
            <br/>
            <br/>Please refer to the page REMINDERS to create a reminder
            </p>
+           </div>
        </div>
     );
 }
