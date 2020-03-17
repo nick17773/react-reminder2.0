6b67ef041ec244311057b9ce291250fd7847ac82
nick17773 <50184147+nick17773@users.noreply.github.com>
2020-03-18 12:48:42 +1300

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Error = () => {
     return (
        <div>
-          <p>Error: Page does not exist!</p>
+          <p>Error: Page does not exist! Try a Different URL.</p>
        </div>
     );
 }
