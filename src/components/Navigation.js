a41bf33f73597759e233cfff367add7babc16f90
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-18 12:17:26 +1300

    1 file modified

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,6 +7,7 @@ const ACTIVE = {background: '#FA6900', color: '#fff'}
 {/* The nav bar function */}
 const Navigation = () => {
     return (
+      <div id="NavBg">
        <div className="Nav">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
@@ -15,6 +16,7 @@ const Navigation = () => {
          
           
        </div>
+      </div>
     );
 }
  
