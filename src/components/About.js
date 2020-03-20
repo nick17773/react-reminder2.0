d9eaaa4ba35ebf5e718d04b782dbc91fe0fb51f5
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-20 13:35:20 +1300

    1 file modified, 1 added, 1 deleted

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const About = () => {
   return (
     <div>
@@ -11,6 +12,7 @@ const About = () => {
       <div className="container">
         <section className="add-item">
           <p>About US page body content</p>
+          <img src="./download.png"/>
         </section>
       </div>
     </div>
