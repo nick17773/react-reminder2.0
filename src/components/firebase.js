9ef39215a6421b5b7da86827cb32990e8345df16
nick17773 <n.a.russell@student.saintkentigern.com>
2020-03-20 13:56:31 +1300

    1 file modified

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,12 +3,15 @@ import 'firebase/database';
 import 'firebase/storage';
 import 'firebase/auth';
 
-var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+var config = {
   apiKey: "AIzaSyAIae2xJEZMbIyaTQ2ht4CnI0UHbJtL6fI",
-  authDomain: "https://react-reminder-2.firebaseapp.com/",
-  databaseURL: "https://react-reminder-2.firebaseio.com/",
+  authDomain: "react-reminder-2.firebaseapp.com",
+  databaseURL: "https://react-reminder-2.firebaseio.com",
+  projectId: "react-reminder-2",
   storageBucket: "react-reminder-2.appspot.com",
-  messagingSenderId: "130650036145"
+  messagingSenderId: "130650036145",
+  appId: "1:130650036145:web:92f9813c054d0081e00f90",
+  measurementId: "G-TL7JX69EE6"
 };
 firebase.initializeApp(config);
 
