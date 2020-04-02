ff409cecc505e64170ba0e05c057b7a3e5452531
nick17773 <n.a.russell@student.saintkentigern.com>
2020-04-03 10:25:35 +1300

    3 files modified

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,15 @@ const About = () => {
     <div>
       <header>
         <div className="wrapper">
-          <h1>About US</h1>
+          <h1>About this project</h1>
         </div>
       </header>
       <div className="container">
         <section className="add-item">
-          <p>About US page body content</p>
+          <p>This project aims to be a reminder app that is usable for students and really anyone at all. <br/>
+          <br/>
+          Note: the date and time do not display as they have been inputted, they will be in yyyy/mm/dd and 24 hour format.
+          </p>
           
           
         </section>
