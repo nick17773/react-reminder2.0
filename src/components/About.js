1df3eb5f090c39ab93ab9404a66fc250133622ce
nick17773 <n.a.russell@student.saintkentigern.com>
2020-04-18 11:22:32 +1200

    1 file modified

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
       </header>
       <div className="container">
         <section className="add-item">
-          <p>The aim of this project is to develop a reminder app to be used by students of other people to remember tasks. <br/>
+          <p>The aim of this project is to develop a reminder app to be used by students or other people to remember tasks. <br/>
           <br/>
           Please note: The date and time on the reminder will display in yyyy/mm/dd and 24 hour format.
           </p>
