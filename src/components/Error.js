97de6c36c219770834098eb1090102f77478ffc9
nick17773 <n.a.russell@student.saintkentigern.com>
2020-05-11 14:43:46 +1200

    1 file modified

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,6 +5,8 @@ const Error = () => {
   return (
     <div className="error">
       <h1>Error 404: Page not found!</h1>
+      <br/>
+      <br/>
       <p>This page does not exist! Please input a different URL or, use <NavLink to="/">This Link</NavLink> To return to the home page.</p>
     </div>
   );
