dadd61b3d7dfed62fa297a8416fc361069c0c9b8
nick17773 <n.a.russell@student.saintkentigern.com>
2020-05-11 14:51:00 +1200

    2 files modified

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,14 +3,22 @@ import { NavLink } from "react-router-dom";
 // this file is for the error message that crops up when the page is non-existant.
 const Error = () => {
   return (
-    <div className="error">
-      <h1>Error 404: Page not found!</h1>
-      <br />
-      <br />
-      <p>
-        This page does not exist! Please input a different URL or, use{" "}
-        <NavLink to="/">This Link</NavLink> To return to the home page.
-      </p>
+    <div>
+      <header>
+        <div className="wrapper">
+          <h1>Error 404: Page not found!</h1>
+        </div>
+        
+      
+      </header>
+      <br/>
+      <section className="error">
+      
+        <p>
+          This page does not exist! Please input a different URL or, use{" "}
+          <NavLink to="/">This Link</NavLink> To return to the home page.
+        </p>
+      </section>
     </div>
   );
 };
